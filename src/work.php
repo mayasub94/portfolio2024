ae8584c8a550f58195d4bdb0f63ef27762b9e013
jjayaraaj <jjayaraaj@gmail.com>
2023-12-27 11:52:39 +0530

    first commit

---
+++ src/work.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maya - work</title>
+    <link href="./../output.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Banner -->
+    <div class=" bg-slate-950 min-h-[60vh]">
+        <div class=" w-container">
+            <div class="flex w-1/2">
+                <h1 class="text-gray-300">I’m Maya </h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- section-2 -->
+    <div class=" min-h-[80vh]"></div>
+
+    <!-- section-3 -->
+    <div class=" min-h-screen bg-slate-950"></div>
+
+</body>
+
+</html>

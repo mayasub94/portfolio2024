662708e21b00a4c753adb4f84c137de9f8403f7f
CB-IT-01-1509 <cb-it-01-1509@CB-IT-01-1509s-MacBook-Pro.local>
2024-03-10 15:55:17 +0530

    updates from maya

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 $(function () {
-  var mainNavigation = `<div class="  flex  justify-between">
+  var mainNavigation = `<div class="  flex  justify-between items-center">
     <div><a href="./"><img src="./images/Logo.svg" alt=""></a></div>
     <ul class="flex text-gray-on-black text-base gap-x-5 font-light">
         <li><a href="./work.html">Work</a></li>
@@ -8,13 +8,25 @@ $(function () {
     </ul>
 </div>`;
 
-  var footer = ` <div class="bg-primary-dark">
-<div class="py-8">
-    <div class="flex text-gray-on-black justify-center items-start gap-x-8">
-        <div>Coded with ❤️️</div>
-        <div>© 2024 All rights reserved</div>
-    </div>
-</div>
+  var footer = `  <div class="bg-primary-dark">
+  <div class="py-8 space-y-6">
+
+      <div class="flex justify-center items-center gap-x-2 text-gray-on-bg text-sm item-middle">
+          <button type="button" class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm text-white shadow-sm hover:bg-white/5">Back to the top</button>
+          • 
+          <button type="button" class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm  text-white shadow-sm hover:bg-white/5">
+          About</button>
+          • 
+          <button type="button" class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm  text-white shadow-sm hover:bg-white/5">Contact</button>
+
+      </div>
+      
+      <div class="flex text-sm text-gray-on-black justify-center items-start gap-x-2">
+          <div> Copyright © 2024 Maya Subramanian.</div>
+          <div>Coded with ❤️️</div>
+          
+      </div>
+  </div>
 </div>`;
 
   $("#footerDiv").html(footer);

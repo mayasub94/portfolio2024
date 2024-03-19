97d44c412ec6f7794a6c5dda6ed761474a78fc3d
jjayaraaj <jjayaraaj@gmail.com>
2024-03-19 08:48:44 +0530

    Updated maya version

--- src/js/app.js
+++ src/js/app.js
@@ -9,25 +9,33 @@ $(function () {
 </div>`;
 
   var footer = `  <div class="bg-primary-dark">
+  <div class="bg-primary-dark">
   <div class="py-8 space-y-6">
 
       <div class="flex justify-center items-center gap-x-2 text-gray-on-bg text-sm item-middle">
-          <button type="button" class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm text-white shadow-sm hover:bg-white/5">Back to the top</button>
-          • 
-          <button type="button" class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm  text-white shadow-sm hover:bg-white/5">
-          About</button>
-          • 
-          <button type="button" class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm  text-white shadow-sm hover:bg-white/5">Contact</button>
+          <button type="button"
+              class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm text-white shadow-sm hover:bg-white/5">Back
+              to the top</button>
+          •
+          <a href="./work.html" type="button"
+              class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm  text-white shadow-sm hover:bg-white/5">Work</a>
+          •
+          <a href="./about.html" type="button"
+              class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm  text-white shadow-sm hover:bg-white/5">About</a>
+          •
+          <a href="./contact.html" type="button"
+              class="rounded-md bg-primary-dark px-2.5 py-1.5 text-sm  text-white shadow-sm hover:bg-white/5">Contact</a>
 
       </div>
-      
+
       <div class="flex text-sm text-gray-on-black justify-center items-start gap-x-2">
           <div> Copyright © 2024 Maya Subramanian.</div>
           <div>Coded with ❤️️</div>
-          
+
       </div>
   </div>
-</div>`;
+</div>
+`;
 
   $("#footerDiv").html(footer);
 
